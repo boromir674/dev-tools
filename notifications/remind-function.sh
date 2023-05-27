dd92c8428dbad5302226d706b780b44f2ba5c45b
konstantinos <boromir674@hotmail.com>
2023-05-27 12:28:56 +0300

    make remind script posix-compliant

--- notifications/remind-function.sh
+++ notifications/remind-function.sh
@@ -1,22 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/sh
 
-# Create a Reminder as a Desktop Notification (pop up)
-# Or Delete a Scheduled Reminder
-
-# Dependencies:
-#   - notify-send (libnotify-bin)
-#   - at (at)
-
-function remind () {
-  local COUNT="$#"
-  local COMMAND="$1"
-  local MESSAGE="$1"
-  local OP="$2"
+remind () {
+  COUNT="$#"
+  COMMAND="$1"
+  MESSAGE="$1"
+  OP="$2"
   shift 2
-  local WHEN="$@"
+  WHEN="$@"
 
   # Display help if no parameters or help command
-  if [[ $COUNT -eq 0 || "$COMMAND" == "help" || "$COMMAND" == "--help" || "$COMMAND" == "-h" ]]; then
+  if [ "$COUNT" -eq 0 ] || [ "$COMMAND" = "help" ] || [ "$COMMAND" = "--help" ] || [ "$COMMAND" = "-h" ]; then
     echo "COMMAND"
     echo "    remind <message> <time>"
     echo "    remind <command>"
@@ -36,36 +29,39 @@ function remind () {
     echo
     return
   fi
+
   # Check presence of AT command
-  if ! which at >/dev/null; then
+  if ! command -v at >/dev/null; then
     echo "remind: AT utility is required but not installed on your system. Install it with your package manager of choice, for example 'sudo apt install at'."
     return
   fi
+
   # Run commands: list, clear
-  if [[ $COUNT -eq 1 ]]; then
-    if [[ "$COMMAND" == "list" ]]; then
+  if [ "$COUNT" -eq 1 ]; then
+    if [ "$COMMAND" = "list" ]; then
       at -l
-    elif [[ "$COMMAND" == "clear" ]]; then
-      at -r $(atq | cut -f1)
+    elif [ "$COMMAND" = "clear" ]; then
+      at -r "$(atq | cut -f1)"
     else
       echo "remind: unknown command $COMMAND. Type 'remind' without any parameters to see syntax."
     fi
     return
   fi
+
   # Determine time of notification
-  if [[ "$OP" == "in" ]]; then
+  if [ "$OP" = "in" ]; then
     # now + count time-units, where the time-units can be minutes, hours, days, or weeks
-    local TIME="now + $WHEN"
-  elif [[ "$OP" == "at" ]]; then
-    local TIME="$WHEN"
-  elif [[ "$OP" == "now" ]]; then
-    local TIME="now"
+    TIME="now + $WHEN"
+  elif [ "$OP" = "at" ]; then
+    TIME="$WHEN"
+  elif [ "$OP" = "now" ]; then
+    TIME="now"
   else
     echo "remind: invalid time operator $OP"
     return
   fi
+
   # Schedule the notification
-  # echo "notify-send '$MESSAGE' 'Reminder' -u critical" | at $TIME 2>/dev/null
-  echo "notify-send '$MESSAGE' 'Reminder'" | at $TIME 2>/dev/null
+  echo "notify-send '$MESSAGE' 'Reminder'" | at "$TIME" 2>/dev/null
   echo "Notification scheduled at $TIME"
 }
